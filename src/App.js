e53d8db2263712a086a9079652babdf68635c8c2
alisarh <alesahhaider@gmail.com>
2021-09-23 10:46:42 +0300

    add routes

--- src/App.js
+++ src/App.js
@@ -1,23 +1,23 @@
-import logo from './logo.svg';
 import './App.css';
+import {Route} from "react-router-dom";
+import HomePage from  './pages/homepage.component';
+const HatsPage = (props) => {
+  console.log({props});
+  return(
+  <div>
+      <h1> HATS PAGE </h1>
+  </div>
+  );
+
+}
+
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+  return (  
+    <div>
+
+       <Route  exact path="/" component= {HomePage} />
+       <Route exact path="/hats" component = { HatsPage} />
     </div>
   );
 }
